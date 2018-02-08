53c8407106f9daecc83acbf97663ed4bdc751371
Gilbert Curbelo <gcurbelo@csumb.edu>
2018-02-08 06:54:39 +0000

    Website

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset = 'utf-8'/>
+        <title> 
+            History of Computers
+        </title>
+        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header id = "name">
+            <h1> History of Computers </h1>
+        </header>
+        <nav>
+            <span id = "currentLink"><a href = "index.php">Overview</a></span>
+            <a href = "1800s.html">1800s</a>
+            <a href = "1900s.html">1900s</a>
+            <a href = "2000s.html">2000s</a>
+        </nav>
+        
+        <main>
+                <figure id = "computer">
+                    <img src = "img/anonymous.png" alt = "anonymous" style="max-width:100%;height:auto;"/>
+                </figure>
+                <div id = "welcomeText">
+<pre>
+You ever wonder where computers come from? You have a wonderful 
+piece of technology available to you, yet most people have no 
+idea how it got to be what it is today. That's why I created 
+this website! :) So, grab a snack, and let's talk history!
+
+The early thought process behind computing systems dates back to 
+the 1800s. An English mathematician named Charles Babbage 
+designed the Analytical Engine that the basic framework of 
+computers are built upon.
+
+
+
+
+<em>"With ordinary talent and extraordanary perseverance, 
+     all things are attainable"</em>
+                    - Thomas F. Buxton
+</pre>
+<br/><br/>
+                </div>
+                
+                <div id = "quote">
+                    
+                </div>
+                
+            
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+                <hr>
+                    &copy; 2018 - Curbelo
+                <br/>
+                <figure id = "logo">
+                    <img src = "img/csumb_logo2.png" alt = "CSUMB Logo" />
+                </figure>
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
